f937f00357867f53b81843ae062d94fb575b8f2f
Kang <wldnjs0wn2@gmail.com>
2022-11-04 18:18:53 +0900

    first commit

---
+++ src/No11382.java
@@ -0,0 +1,23 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No11382 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(" ");
+		
+
+		long a=Long.parseLong(str[0]);
+		long b=Long.parseLong(str[1]);
+		long c=Long.parseLong(str[2]);
+		
+		
+		System.out.println(a+b+c);
+	}
+
+}
